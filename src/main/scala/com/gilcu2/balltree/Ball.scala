b470f59fc7b89c3f7f518f5abc5f19e6711baf95
Reynaldo Gil <gilcu2@gmail.com>
2019-08-30 12:58:35 +0100

    computing bounding ball done

--- src/main/scala/com/gilcu2/balltree/Ball.scala
+++ src/main/scala/com/gilcu2/balltree/Ball.scala
@@ -19,5 +19,8 @@ case class Ball[T](center: T, radio: Double = 0) {
   def maximumDistance(b: Ball[T])(implicit space: Space[T]): Double =
     space.distance(this.center, b.center) + this.radio + b.radio
 
+  def maximumDistance(p: T)(implicit space: Space[T]): Double =
+    space.distance(this.center, p) + this.radio
+
 
 }
--- src/main/scala/com/gilcu2/balltree/BallTree.scala
+++ src/main/scala/com/gilcu2/balltree/BallTree.scala
@@ -34,19 +34,29 @@ class BallTree[T](space: Space[T], balls: Ball[T]*) {
     val b1 = findFarthestFrom(balls(0), balls)
     val b2 = findFarthestFrom(b1, balls)
 
-    val center = midPoint(b1, b2)
-    val radio = space.distance(center, b2.center) + b2.radio
+    var center = midPoint(b1, b2)
+    var radio = space.distance(center, b2.center) + b2.radio
     var boundingBall = Ball(center, radio)
 
     var ballsOutside = balls.filter(!boundingBall.contains(_))
-    while (balls.nonEmpty) {
-      val farthest =
+    while (ballsOutside.nonEmpty) {
+      val farthest = findFarthestFrom(center, balls)
+      val distance = farthest.maximumDistance(center)
+      val delta = distance - radio
+      center = space.moveToward(center, farthest.center, delta)
+      radio = radio * 1.01
+      boundingBall = Ball(center, radio)
+      ballsOutside = balls.filter(!boundingBall.contains(_))
     }
 
+    boundingBall
   }
 
   def findFarthestFrom(b: Ball[T], balls: Seq[Ball[T]]): Ball[T] =
     balls.map(b1 => (b1, b.minimumDistance(b1))).maxBy(_._2)._1
+
+  def findFarthestFrom(p: T, balls: Seq[Ball[T]]): Ball[T] =
+    balls.map(b1 => (b1, b1.maximumDistance(p))).maxBy(_._2)._1
 
   def midPoint(b1: Ball[T], b2: Ball[T]): T = {
     val distance = b1.minimumDistance(b2)
