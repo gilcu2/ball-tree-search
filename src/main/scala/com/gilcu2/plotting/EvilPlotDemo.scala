d77879ee577510f247c8a76fa3c481613c6af939
Reynaldo Gil <gilcu2@gmail.com>
2019-09-29 06:08:10 +0100

    beginning plot discs

--- src/main/scala/com/gilcu2/plotting/EvilPlotDemo.scala
+++ src/main/scala/com/gilcu2/plotting/EvilPlotDemo.scala
@@ -3,7 +3,7 @@ package com.gilcu2.plotting
 import java.awt.Image.SCALE_SMOOTH
 
 import com.cibo.evilplot.colors._
-import com.cibo.evilplot.geometry.Drawable
+import com.cibo.evilplot.geometry.{Disc, Drawable}
 import scalaview.Utils._
 import com.cibo.evilplot.numeric.{Datum2d, Point, Point3d}
 import com.cibo.evilplot.plot._
@@ -18,7 +18,8 @@ object EvilPlotDemo {
   def main(args: Array[String]): Unit = {
 
     //    val plot=ScatterPlotDemo
-    val plot = PointRendererDemo
+    //    val plot = PointRendererDemo
+    val plot = CircleRendererDemo
 
     scalaview.SfxImageViewer(biResize(plot.asBufferedImage, 1000, 800, SCALE_SMOOTH))
 
@@ -54,5 +55,27 @@ object EvilPlotDemo {
       .render()
 
   }
+
+  private def CircleRendererDemo = {
+
+    val visibleColor = HSLA(100, 100, 100, 1)
+    val transparentColor = HSLA(210, 100, 56, 0.1)
+    Random.setSeed(666L) //evil seed
+    val data = Seq.fill(10)(
+      Point3d(1000 * Random.nextDouble(), 1000 * Random.nextDouble(), Random.nextInt(100)))
+
+    ScatterPlot(
+      data,
+      pointRenderer = Some(PointRenderer.custom({ x: Point3d[Int] =>
+        Disc.centered(x.z).filled(transparentColor).colored(visibleColor).weighted(2)
+      }))
+    ).xAxis()
+      .yAxis()
+      .frame()
+      .rightLegend()
+      .render()
+
+  }
+
 
 }
